3666d5f58662e783d0f1f5c4e27d89492ffb9dbd
JC <chohy1106@gmail.com>
2023-02-21 17:02:02 +0900

    Feat countries card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Countries from "./pages/Countries";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Countries />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
