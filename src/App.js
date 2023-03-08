865530c4a24bd3aa680785734df400f91a0f5638
avocad06 <miggo2704@gmail.com>
2023-03-08 15:13:38 +0900

    feat: navbar추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Countries from "./pages/Countries/Countries";
-import Community from "./pages/Community/Community";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 import Review from "./pages/Review/Review";
-import { countryList } from "./util/countyList";
+import Countries from "./pages/Countries/Countries";
+import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/account",
+    element: <div>회원가입 영역입니다.</div>,
+  },
+  {
+    path: "/",
+    element: <Countries />,
+  },
+  {
+    path: "/:id",
+    element: <Home />,
+    children: [
+      {
+        path: "review/:reviewId",
+        element: <Review />,
+      },
+      {
+        path: "",
+        element: <div>국가영역입니다.</div>
+      }
+    ]
+  }
+])
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Countries />}></Route>
-        <Route path="/community" element={<Community />}></Route>
-        <Route path="/review/:id" element={<Review />}></Route>
-      </Routes>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import { Link, Outlet } from 'react-router-dom'
+import style from './Navbar.module.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineSearch } from "react-icons/ai";
+import { useState } from 'react';
+
+const linkList = [
+    {
+        name: '메인',
+        path: '/'
+    },
+    {
+        name: '커뮤니티',
+        path: '/'
+    },
+    {
+        name: '내 스케줄',
+        path: '/'
+    },
+    {
+        name: '로그인',
+        path: '/'
+    },
+]
+
+
+function Navbar() {
+
+    const [showLinks, setShowLinks] = useState(false);
+
+    return (
+        <nav className={style.Navbar}>
+            <header className={style.header}>
+                <Link to='/' className={style.logo}>
+                    <img src={process.env.PUBLIC_URL + '/assets/logo.png'} />
+                </Link>
+
+                <div className={style.btn_wrapper}>
+                    <button className={style.search}>
+                        <AiOutlineSearch />
+                    </button>
+                    <button className={style.toggle} onClick={() => setShowLinks(!showLinks)}>
+                        <GiHamburgerMenu />
+                    </button>
+                </div>
+
+                <div className={showLinks ? `${style.menu} ${style.open}` : `${style.menu}`}>
+                    <ul>
+                        {linkList.map((link, index) =>
+                        (<li key={index}>
+                            <Link to={link.path}>{link.name}</Link>
+                        </li>)
+                        )}
+                    </ul>
+                </div>
+            </header>
+        </nav>
+    )
+}
+
+export default Navbar
