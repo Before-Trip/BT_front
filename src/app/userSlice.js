229ec0fff550ee2e3ebee4a89563282637a7281d
avocad06 <miggo2704@gmail.com>
2023-03-11 15:58:26 +0900

    feat: user 상태 전역관리 추가

---
+++ src/app/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'userInfo',
+    initialState: {
+        name: "",
+        id: null,
+        email: null,
+        isLogined: null,
+        isLoading: false,
+    },
+    reducers: {
+        login: (state, action) => {
+            state.email = action.payload.email;
+            state.isLogined = true;
+            console.log("로그인 되었습니다.")
+        },
+
+        logoutUser: (state) => {
+            state.name = "";
+            state.email = "";
+            state.isLogined = false;
+        }
+    }
+})
+
+export const { login, logoutUser } = userSlice.actions
+export default userSlice.reducer
