d870b3a38e3cb5105dee64176d811590e0327741
avocad06 <miggo2704@gmail.com>
2023-03-15 00:20:02 +0900

    fix: 프론트 쿠키set 로직 추가

--- src/utils/getRefresh.js
+++ src/utils/getRefresh.js
@@ -1,25 +1,24 @@
 import { BASE_URL } from "./const"
+import { getCookieToken } from "./cookies"
 
 export const getRefresh = async () => {
-    const refreshToken = document.cookie.match(new RegExp("(?:^|; )" + "refresh".replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"))[1] // eslint-disable-line
-
-    if (refreshToken) {
-        const refreshTokenRes = await fetch(`${BASE_URL}users/auth/refresh/`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                // 'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                refresh: refreshToken
-            })
+    const refreshToken = getCookieToken();
+    console.log(refreshToken)
+    const refreshTokenRes = await fetch(`${BASE_URL}users/auth/refresh/`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            // 'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            refresh: refreshToken
         })
+    })
 
-        if (refreshTokenRes.ok) {
-            const Token = await refreshTokenRes.json()
-            console.log(Token)
-            return Token.access
-        }
+    if (refreshTokenRes.ok) {
+        const Token = await refreshTokenRes.json()
+        console.log(Token)
+        return Token.access
     }
 }
